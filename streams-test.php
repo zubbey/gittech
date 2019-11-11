da58728568f93478747737fd386ff4ca7637e10f
zubbey <56047720+zubbey@users.noreply.github.com>
2019-11-11 19:37:39 +0100

    just for testing purpose

---
+++ streams-test.php
@@ -0,0 +1,26 @@
+  $email = 'com.zubbey@hotmail.com';
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, 'https://api.paystack.co/customer/'.$email);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+
+  $headers = array();
+  $headers[] = 'Authorization: Bearer sk_test_f89bb31f1bda1cdb1f77d255987843b82f1a8e56';
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $result = curl_exec($ch);
+  if($result){
+
+    $cusdata = json_decode($result);
+
+    echo "<pre>";
+    echo var_dump($cusdata);
+    echo "</pre>";
+  }
+
+  if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+  }
+  curl_close($ch);
